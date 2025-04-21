603f941737dc8d7de5a9fc6ae567d95f3b68c9df
NAM TOLA <namtola4444@gmail.com>
2025-04-20 23:46:06 -0800

    Apply .gitignore rules and clean repo

--- .history/servers/admin/connections/connectiondatabase_20250412234553.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function connectiondatabase(): mysqli
-{
-    return new mysqli("Localhost", "root", "", "system_online_shop");
-}
-?>
--- .history/servers/admin/connections/connectiondatabase_20250420231611.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function connection_database(): mysqli
-{
-    return new mysqli("Localhost", "root", "", "system_online_shop");
-}
-?>
